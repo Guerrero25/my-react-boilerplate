8b70871bbc5c18bbbc19ebcc4b89e8250c935bdc
Omar Guerrero <guerrero9725@gmail.com>
2019-03-25 10:44:49 -0500

    Initial commit v0.1.0

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+/* Styles */
+import "./style.scss";
+
+class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
